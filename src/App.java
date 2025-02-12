107c5185768b360f9b994ecfa7a6ee8471f94046
wyctorfogos <wyctor.fogos@gmail.com>
2025-02-12 13:01:54 -0300

    perf: Remoção do print

--- src/App.java
+++ src/App.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        // System.out.println("Hello, World!");
         System.out.println("Let's register our new car!");
 
         Scanner scanner = new Scanner(System.in);
